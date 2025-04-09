bb6bc9fed995f0f407879e2dc2edc6ef11049dde
SOUGATA SAHA <sahasougata820@gmail.com>
2025-04-10 01:24:53 +0530

    some change

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-      <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx- sm:px-10 px-5">
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.9] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
